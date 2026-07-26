97934f22c7fa352699fba443e196c8f9ea4e88d5
johnny <dark_skateboarding2@hotmail.com>
2026-07-26 13:33:08 +0300

    chore: change error message

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  username: z.email({ error: "Μη έγκυρο email" }),
-  password: z.string().min(6, { error: "Ο κωδικός πρέπει να έχει τουλάχιστον 6 χαρακτήρες" })
+  username: z.email({ error: "Invalid email" }),
+  password: z.string().min(6, { error: "Password must contain at least 6 characters" })
 });
 
 export const signupSchema = z.object({
-  username: z.email({ error: "Μη έγκυρο email" }),
-  password: z.string().min(6, { error: "Ο κωδικός πρέπει να έχει τουλάχιστον 6 χαρακτήρες" })
+  username: z.email({ error: "invalid email" }),
+  password: z.string().min(6, { error: "Password must contain at least 6 characters" })
 });
