e8049cd0107d11d23266ca48aa9edd22a17290a3
johnny <dark_skateboarding2@hotmail.com>
2026-07-26 12:10:08 +0300

    chore: configure path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +9,9 @@ export default defineConfig({
       react(),
       tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
