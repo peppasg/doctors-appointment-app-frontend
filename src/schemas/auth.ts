f0f60a767a1e551b3b60c803640fe7ed295dc50f
johnny <dark_skateboarding2@hotmail.com>
2026-08-12 19:42:26 +0300

    fix(schema): change to capital I

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -5,7 +5,9 @@ export const loginSchema = z.object({
   password: z.string().min(6, { error: "Password must contain at least 6 characters" })
 });
 
+export type LoginFields = z.infer<typeof loginSchema>;
+
 export const signupSchema = z.object({
-  username: z.email({ error: "invalid email" }),
+  username: z.email({ error: "Invalid email" }),
   password: z.string().min(6, { error: "Password must contain at least 6 characters" })
 });
