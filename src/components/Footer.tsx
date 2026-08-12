d50d8b7eec872106d38682c5b63474f2e94ccac5
johnny <dark_skateboarding2@hotmail.com>
2026-08-12 19:46:00 +0300

    chore: fix code indentation on header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,15 @@
 const Footer = () => {
     const currentYear: number = new Date().getFullYear();
     return (
-        <>
-            <footer className="bg-gray-700 text-white font-medium">
-                <div className="container mx-auto py-6 text-center">
-                    &copy; {currentYear} Coding Factory 9. All Rights reserved.
-                </div>
-            </footer>
-        </>
-    )
-}
-export default Footer;
+      <>
+        <footer className="bg-gray-700 text-white font-medium">
+          <div className="container mx-auto py-6 text-center">
+            &copy; {currentYear} Coding Factory 9. All Rights reserved.
+          </div>
+        </footer>
+      </>
+    );
+  };
+  
+  export default Footer;
+  
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <>
-            <header className="bg-gray-700 h-16 fixed w-full z-50">
-                <div className="container mx-auto px-5 flex items-center justify-between">
-                    <Link className="flex" to="/">
-                        <img
-                            className="h-16 w-auto object-contain invert mix-blend-screen"
-                            src="../../public/heart-pulse.svg"
-                            alt="Doctors Logo"
-                        />
-                        <p className="py-5 text-white font-medium">Medical Appointment Application</p>
-                    </Link>
-                    <nav className="flex gap-4 text-white">
-                        <Link to="/">Home</Link>
-                        <Link to="/login">Login</Link>
-                    </nav>
-                </div>
-            </header>
-        </>
-    )
-}
+  return (
+    <>
+      <header className="bg-gray-700 h-16 fixed w-full z-50">
+        <div className="container mx-auto px-5 flex items-center justify-between">
+          <Link className="flex" to="/">
+            <img
+              className="h-16 w-auto object-contain invert mix-blend-screen"
+              src="/heart-pulse.svg" 
+              alt="Doctors Logo"
+            />
+            <p className="py-5 text-white font-medium">
+              Medical Appointment Application
+            </p>
+          </Link>
+          <nav className="flex gap-4 text-white">
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
 export default Header;
