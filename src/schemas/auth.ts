e73f46e684d7555b4b48b065f068fb858fc98bad
johnny <dark_skateboarding2@hotmail.com>
2026-08-26 21:37:36 +0300

    fix(schemas): correct minimum characters and error messages

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  username: z.email({ error: "Invalid email" }),
-  password: z.string().min(6, { error: "Password must contain at least 6 characters" })
+  username: z.string().min(3, { error: "Username must contain at least 3 characters" }),
+  password: z.string().min(5, { error: "Password must contain at least 5 characters" })
 });
 
 export type LoginFields = z.infer<typeof loginSchema>;
 
 export const signupSchema = z.object({
-  username: z.email({ error: "Invalid email" }),
-  password: z.string().min(6, { error: "Password must contain at least 6 characters" })
+  username: z.string().min(3, { error: "Username must contain at least 3 characters" }),
+  password: z.string().min(5, { error: "Password must contain at least 5 characters" })
 });
